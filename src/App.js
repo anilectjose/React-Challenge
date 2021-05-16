e4c867f5546eb8468828c674e3cfbf59f70785bf
anilectjose <anilectjose2022a@ajce.mca.in>
2021-05-16 14:21:08 +0530

    Content added

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import './App.css';
 import Header from './Components/Header';
 import ClassComponent from './Components/Class';
 function App() {
-  const abc='hello I\'m Anilect Jose';
+  const abc = 'hello I\'m Anilect Jose';
   return (
-    <div style={{background:'lightblue'}}>
-<Header data={abc}/>      
-<h1 style={{backgroundColor:'red',color:'yellow'}}>Inline CSS</h1>
-<p>Simple P Tag</p>
-<p className='hello'>Hai World this is for external class</p>
-<p className={'hello'}>Hai World this is for external class with looping option</p>
-<ClassComponent detail={abc}/>
-<Babu/>
-</div>
+    <div style={{ background: 'lightblue' }}>
+      <Header data={abc} />
+      <h1 style={{ backgroundColor: 'red', color: 'yellow' }}>Inline CSS</h1>
+      <p>Simple P Tag</p>
+      <p className='hello'>Hai World this is for external class</p>
+      <p className={'hello'}>Hai World this is for external class with looping option</p>
+      <ClassComponent detail={abc} />
+      <Babu />
+    </div>
   );
 }
 
 export default App;
 
 function Babu() {
-  return(
-  <h3> This is internal Function in main page</h3>
-)
+  return (
+    <h3> This is internal Function in main page</h3>
+  )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
 function Header(props) {
     return(
-        <h1>This is the Header Part of application {props.data}</h1>
+        <h1>This is the Header Part of application with value= {props.data}</h1>
     )
 }
 
